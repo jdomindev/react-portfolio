41c341a31273e3f2e88f1bc943bdbc0e9d11e130
Jose Dominguez <jdomindev@gmail.com>
2022-02-16 17:05:34 -0500

    fixed project.js to show projects in a cleaner layout

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -5,15 +5,17 @@ export default function Footer() {
         <footer className="text-center text-white" id="contact">
             <div className="container p-4 pb-0">
                 <section className="mb-1">
-                <a className="btn btn-outline-light btn-floating me-2" href="mailto:jdomindev@gmail.com" role="button"
+                <a className="btn btn-outline-light btn-floating me-2" href="mailto:jdomindev@gmail.com" target="_blank" rel="noreferrer" role="button"
                     ><i className="fab fa-google"></i
                 ></a>
-                <a className="btn btn-outline-light btn-floating me-2" href="https://www.linkedin.com/in/jose-dominguez-94b689225/" role="button"
+                <a className="btn btn-outline-light btn-floating me-2" href="https://www.linkedin.com/in/jose-dominguez-94b689225/" target="_blank" rel="noreferrer" role="button"
                     ><i className="fab fa-linkedin"></i
                 ></a>
-                <a className="btn btn-outline-light btn-floating me-2" href="https://github.com/jdomindev" role="button"
+                <a className="btn btn-outline-light btn-floating me-2" href="https://github.com/jdomindev" target="_blank" rel="noreferrer" role="button"
                     ><i className="fab fa-github"></i
                 ></a>
+                <a className="btn btn-outline-light btn-floating me-2" href="https://stackoverflow.com/users/17254557/jose-dominguez" target="_blank" rel="noreferrer" role="button"
+                    ><i class="fab fa-stack-overflow"></i></a>
                 </section>
             </div>
             <div className="mt-3">
@@ -23,5 +25,5 @@ export default function Footer() {
                 <h6 className="text-white text-decoration-none">© 2022 Copyright: Jose Dominguez</h6>
             </div>
         </footer>
-    );
+    )
 }
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,41 +3,47 @@ import '../../styles/Project.css'
 // import json
 import projects from '../../projects.json'
 
+const projectList = projects.map(project => (
+    <a className="project sub-project" style={{ backgroundImage: project.image}} title="screenshot of project" href={project.deployed} target="_blank" rel="noreferrer" key={project.id}>
+        <h3 className="project-heading">{project.title}</h3>
+        <form action={project.repo} target="_blank" method="get">
+        <button className="repo-heading"><i className="fab fa-github"></i></button></form>
+    </a>
+))
+
 export default function Project() {
-  return (
-      
-    //   map over json
-    // {projects.map(project => (
-    //     <a className="project project-background project-background-img-2 sub-project"  title="screenshot of project" href={project.deployed} target="_blank" rel="noreferrer" key={project.id}>
-    //         <h3 className="project-heading">{project.title}</h3>
-    //         <form action={project.repo} target="_blank" method="get">
-    //         <button className="repo-heading"><i className="fab fa-github"></i></button></form>
-    //     </a>
-    // ))}
+    return (
+    <section className="section-container m-5">
+        <h2 className="text-center text-white project-header m-5 p-3">Projects</h2>
+        <section className="project-container">
+            { projectList }
+        </section>
+    </section>
+    
     // <section className="section-container">
     //         <div className="header-container" id="projects">
     //           <h2>Projects</h2>
     //         </div>
-        <section className="project-container m-5">
-                <a className="project project-background-img-1 main-project" title="screenshot of project" href="https://jdomindev.github.io/game-getter/" target="_blank" rel="noreferrer">
-                    <h3 className="project-heading">Game Getter</h3>
-                    <form action="https://github.com/jdomindev/game-getter" target="_blank" method="get"><button className="repo-heading"><i className="fab fa-github"></i></button></form>
-                </a>
-                <div className="sub-projects">
-                    <a className="project project-background-img-2 sub-project" title="screenshot of project" href="https://jdomindev.github.io/work-day-planner/" target="_blank" rel="noreferrer">
-                        <h3 className="project-heading">Work Day Planner</h3>
-                        <form action="https://github.com/jdomindev/work-day-planner" target="_blank" method="get"><button className="repo-heading"><i className="fab fa-github"></i></button></form>
-                    </a>
-                    <a className="project project-background-img-3 sub-project" title="screenshot of project" href="https://jdomindev.github.io/password-generator/" target="_blank" rel="noreferrer">
-                        <h3 className="project-heading">Password Generator</h3>
-                        <form action="https://github.com/jdomindev/password-generator" target="_blank" method="get"><button className="repo-heading"><i className="fab fa-github"></i></button></form>
-                    </a>
-                    <a className="project project-background-img-4 sub-project" title="screenshot of project" href="https://jdomindev.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
-                        <h3 className="project-heading">Weather Dashboard</h3>
-                        <form action="https://github.com/jdomindev/weather-dashboard" target="_blank" method="get"><button className="repo-heading repo-button"><i className="fab fa-github"></i></button></form>
-                    </a>
-                </div>
-        </section>
-        // </section>
+    //     <section className="project-container m-5">
+    //             <a className="project project-background-img-1 main-project" title="screenshot of project" href="https://jdomindev.github.io/game-getter/" target="_blank" rel="noreferrer">
+    //                 <h3 className="project-heading">Game Getter</h3>
+    //                 <form action="https://github.com/jdomindev/game-getter" target="_blank" method="get"><button className="repo-heading"><i className="fab fa-github"></i></button></form>
+    //             </a>
+    //             <div className="sub-projects">
+    //                 <a className="project project-background-img-2 sub-project" title="screenshot of project" href="https://jdomindev.github.io/work-day-planner/" target="_blank" rel="noreferrer">
+    //                     <h3 className="project-heading">Work Day Planner</h3>
+    //                     <form action="https://github.com/jdomindev/work-day-planner" target="_blank" method="get"><button className="repo-heading"><i className="fab fa-github"></i></button></form>
+    //                 </a>
+    //                 <a className="project project-background-img-3 sub-project" title="screenshot of project" href="https://jdomindev.github.io/password-generator/" target="_blank" rel="noreferrer">
+    //                     <h3 className="project-heading">Password Generator</h3>
+    //                     <form action="https://github.com/jdomindev/password-generator" target="_blank" method="get"><button className="repo-heading"><i className="fab fa-github"></i></button></form>
+    //                 </a>
+    //                 <a className="project project-background-img-4 sub-project" title="screenshot of project" href="https://jdomindev.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
+    //                     <h3 className="project-heading">Weather Dashboard</h3>
+    //                     <form action="https://github.com/jdomindev/weather-dashboard" target="_blank" method="get"><button className="repo-heading repo-button"><i className="fab fa-github"></i></button></form>
+    //                 </a>
+    //             </div>
+    //     </section>
+    // </section>
   );
 }
