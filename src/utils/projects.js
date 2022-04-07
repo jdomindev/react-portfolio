74b946e96b87e0d74a3690008401c9c727488fb4
Jose Dominguez <jdomindev@gmail.com>
2022-04-07 17:50:52 -0400

    added project descriptions to projects json

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,6 +2,7 @@ const projects = [
     {
         "id": 1,
         "title": "Game Getter",
+        "description": "Find the best deal for the game you desire.",
         "repo": "https://github.com/jdomindev/game-getter",
         "deployed": "https://jdomindev.github.io/game-getter/",
         "image": require("../images/project-image-1.png")
@@ -9,6 +10,7 @@ const projects = [
     {
         "id": 2,
         "title": "Work Day Planner",
+        "description": "Day scheduler that renders HTML dynamically using jQuery and Bootstrap. The app also utilizes Moment library and stores values in local storage.",
         "repo": "https://github.com/jdomindev/work-day-planner",
         "deployed": "https://jdomindev.github.io/work-day-planner/",
         "image": require("../images/project-image-2.png")
@@ -16,6 +18,7 @@ const projects = [
     {
         "id": 3,
         "title": "Password Generator",
+        "description": "Generator that takes in user input for password prerequisites and then generates a random password on the page.",
         "repo": "https://github.com/jdomindev/password-generator",
         "deployed": "https://jdomindev.github.io/password-generator/",
         "image": require("../images/project-image-3.png")
@@ -23,6 +26,7 @@ const projects = [
     {
         "id": 4,
         "title": "Weather Dashboard",
+        "description": "Weather app that utilizes data fetched from OpenWeatherMap API.",
         "repo": "https://github.com/jdomindev/weather-dashboard",
         "deployed": "https://jdomindev.github.io/weather-dashboard/",
         "image": require("../images/project-image-4.png")
@@ -30,15 +34,17 @@ const projects = [
     {
         "id": 5,
         "title": "Tech Blog",
+        "description": "Tech blog built out following the MVC paradigm and utilizing Handlebars.js, Sequelize, and express-session for authentication.",
         "repo": "https://github.com/jdomindev/tech-blog",
         "deployed": "https://secret-everglades-34739.herokuapp.com/",
         "image": require("../images/project-image-5.png")
     },
     {
         "id": 6,
-        "title": "g-Wish",
-        "repo": "https://github.com/jdomindev/g-Wish",
-        "deployed": "https://afternoon-stream-65311.herokuapp.com/",
+        "title": "Tandom-Fandom",
+        "description": "A people finding app designed for those seeking others with similar fandom interests.",
+        "repo": "https://github.com/jdomindev/tandemfandom",
+        "deployed": "https://tandemfandom.herokuapp.com/",
         "image": require("../images/project-image-6.png")
     }
 ]
